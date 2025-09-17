ee42df0061df0ad4d9588f92bc036c0b025e3de2
Denner Dangelo Aladim de Castro <dennerdangelo57@gmail.com>
2025-09-17 15:56:41 -0300

    Atualizado a Pagina Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,24 +3,29 @@ import type { JSX } from "react";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import { Link } from "react-router-dom";
+import Container from "../../components/Container";
 
-const Home = ():JSX.Element => {
+import styles from "./Home.module.scss";
+
+const Home = (): JSX.Element => {
     return (
         <>
             <Header />
-            <section className='container'>
-                <div className='apresentacao'>
-                    <p>
-                        Olá sou,<br />
-                        <span>Denner Aladim</span><br />
-                        Dev Front End Júnior
-                    </p>
-                    <Link to="/sobre" className='btn btn-red'>Saiba mais</Link>
-                </div>
-                <figure>
-                    <img className='img-home' src="./developer_blue.svg" alt="Imagem de Home" />
-                </figure>
-            </section>
+            <Container>
+                <section className={styles.home}>
+                    <div className={styles.apresentacao}>
+                        <p>
+                            Olá sou,<br />
+                            <span>Denner Aladim</span><br />
+                            Dev Front End Júnior
+                        </p>
+                        <Link to="/sobre" className={`${styles.btn} ${styles['btn-red']}`}>Saiba mais</Link>
+                    </div>
+                    <figure>
+                        <img src="./developer_blue.svg" alt="Imagem de Home" />
+                    </figure>
+                </section>
+            </Container>
             <Footer />
         </>
     )
