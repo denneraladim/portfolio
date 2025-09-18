0cfde7b0afda3fe31e9f591a3b57dafb767b43e6
Denner Dangelo Aladim de Castro <dennerdangelo57@gmail.com>
2025-09-18 20:09:04 -0300

    Atualizado a Pagina de Projetos

--- src/pages/Projetos/index.tsx
+++ src/pages/Projetos/index.tsx
@@ -1,9 +1,13 @@
 
+import Card from "../../components/Card";
+
 const Projetos = () => {
     return (
         <>
-
             <h2>Projetos</h2>
+            <Card/>
+            <Card/>
+            <Card/>
         </>
     )
 }
