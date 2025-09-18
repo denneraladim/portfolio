bd8e9b84dd89e62368c63fbe025ff4aac76b9f6c
Denner Dangelo Aladim de Castro <dennerdangelo57@gmail.com>
2025-09-18 15:35:49 -0300

    Atualizado o arquivo Routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,24 +5,21 @@ import Sobre from "./pages/Sobre";
 import Contatos from "./pages/Contatos";
 import Projetos from "./pages/Projetos";
 import NotFound from "./pages/NotFound";
-import Header from "./components/Header";
-import Container from "./components/Container";
-import Footer from "./components/Footer";
+import PageBase from "./pages/PageBase";
+
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
-        <Header/>
-        <Container>
             <Routes>
-                <Route path="/" element={ <Home/>}/>
-                <Route path="/sobre" element={ <Sobre/>}/>
-                <Route path="/projetos" element={ <Projetos/>}/>
-                <Route path="/contatos" element={ <Contatos/>}/>
-                <Route path="*" element={ <NotFound/>}/>
+                <Route path="/" element={<PageBase/>}>
+                    <Route index element={ <Home/>}/>
+                    <Route path="/sobre" element={ <Sobre/>}/>
+                    <Route path="/projetos" element={ <Projetos/>}/>
+                    <Route path="/contatos" element={ <Contatos/>}/>
+                    <Route path="*" element={ <NotFound/>}/>
+                    </Route>
             </Routes>
-            </Container>
-            <Footer/>
         </BrowserRouter>
     )
 }
