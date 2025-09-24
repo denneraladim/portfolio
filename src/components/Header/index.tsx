a85caef16084fe4e72aae94e0e4ad2985d92f80f
Denner Dangelo Aladim de Castro <dennerdangelo57@gmail.com>
2025-09-24 10:35:37 -0300

    Criado o Menu Sandwich

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,22 +2,33 @@
 
 import { Link } from "react-router-dom";
 import styles from "./Header.module.scss";
-import type { JSX } from "react";
+import { useState, type JSX } from "react";
 
 const Header = (): JSX.Element => {
+
+    const [showMenu, setMenu] = useState(false);
+    const toggleMenu = () => {
+        setMenu(!showMenu);
+    }
+
     return (
         <header className={styles.header}>
             <Link to="/" className={styles.logo}>
                 <span>dennerAladim.dev</span>
             </Link>
             
-            <nav>
+            <nav
+             className={`${styles.menuSandwich} ${showMenu ? styles.show : ""}`}
+             >
                 <Link to="/">Home</Link>
                 <Link to="/sobre">Sobre</Link>
                 <Link to="/projetos">Projetos</Link>
                 <Link to="/contatos">Contatos</Link>
             </nav>
-            <div className={styles.menuButton}>
+            <div
+             className={styles.menuButton}
+             onClick={toggleMenu}
+             >
                 <span className={styles.linha}></span>
                 <span className={styles.linha}></span>
                 <span className={styles.linha}></span>
